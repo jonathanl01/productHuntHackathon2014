abc10cc56067443e34e74e1debd3166a48565b42
Jonathan Lei <jonathan.lei01@gmail.com>
2014-10-25 23:48:56 -0700

    keep keyboard down on startup

--- app/src/main/java/com/example/jonathanlei/producthuntandroidapp/MainActivity.java
+++ app/src/main/java/com/example/jonathanlei/producthuntandroidapp/MainActivity.java
@@ -1,32 +1,10 @@
 package com.example.jonathanlei.producthuntandroidapp;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
+import android.view.WindowManager;
 
 
 public class MainActivity extends Activity {
@@ -35,6 +13,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
